07ab6171a5d3e7592a9bb5c20547642da5ae80b1
Taddes Korris <taddes.korris@passportinc.com>
2018-10-18 15:18:27 -0400

    updates to biolerplate

--- boilerplate/src/input.js
+++ boilerplate/src/input.js
@@ -9,3 +9,31 @@ class Hangman {
 
 const hangman = new Hangman()
 console.log(hangman.myMethod())
+
+const calculateAverage = (numOne, numTwo) => {
+  return (numOne + numTwo) / 2
+}
+
+console.log(calculateAverage(0, 100))
+
+// Rest parameter - dynamic array handling
+// Works regardless of the number of args you pass in!
+const calculateAverageRest = (...numbers) => {
+  let sum = 0;
+  numbers.forEach((num) => sum += num);
+  return sum / numbers.length;
+}
+
+console.log(calculateAverageRest(45, 66, 46, 78));
+
+// To grab subset of arguments
+const calculateAverageRestSubset = (thing, ...numbers) => {
+  let sum = 0;
+  numbers.forEach((num) => sum += num);
+  let average =  sum / numbers.length;
+  return `The average ${thing} is ${average}`;
+}
+
+console.log(calculateAverageRestSubset('Age', 65, 54, 67, 87));
+
+// 
