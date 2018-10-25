a7b646affea64a968ee9e5be04512c500e6fe181
Taddes Korris <taddes.korris@passportinc.com>
2018-10-25 15:45:55 -0400

    refractor

--- notes-app/src/edit.js
+++ notes-app/src/edit.js
@@ -1,15 +1,15 @@
 import { initializeEditPage, generateLastEdited } from './views';
 import { updateNote, removeNote } from './notes';
 
-const titleElement = document.querySelector('#note-title')
-const bodyElement = document.querySelector('#note-body')
-const removeElement = document.querySelector('#remove-note')
-const dateElement = document.querySelector('#last-edited')
+const titleElement = document.querySelector('#note-title');
+const bodyElement = document.querySelector('#note-body');
+const removeElement = document.querySelector('#remove-note');
+const dateElement = document.querySelector('#last-edited');
 
 // Capture the note-hash corresponding to note ID
-const noteId = location.hash.substring(1)
+const noteId = location.hash.substring(1);
 
-initializeEditPage(noteId)
+initializeEditPage(noteId);
 
 //Set up input event for input, text area. 
 titleElement.addEventListener('input', (e) => { 
@@ -21,18 +21,18 @@ titleElement.addEventListener('input', (e) => {
 
 bodyElement.addEventListener('input', (e) => {
   const note = updateNote(noteId, {
-    body: e.target.body
+    body: e.target.body;
   })
-  dateElement.textContent = generateLastEdited(note.updatedAt)
+  dateElement.textContent = generateLastEdited(note.updatedAt);
 });
 
 removeElement.addEventListener('click', (e) => {
-  removeNote(note.id)
+  removeNote(nnoteId);
   location.assign('/index.html')
 });
 
 window.addEventListener('storage', (e) => {
   if (e.key === 'notes') {
-    initializeEditPage(noteId)
+    initializeEditPage(noteId);
   }
 });
